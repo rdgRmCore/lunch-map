e3da9f16433e5ae72b41aea14cd20325691d95f6
rdg <rdg@rmcore.com>
2015-08-19 16:24:46 -0500

    Add knock out js library and display a list of restaurants.

--- js/map.js
+++ js/map.js
@@ -1,3 +1,17 @@
+// Whole-script strict mode syntax
+"use strict";
+
+var initialRestaurants = [
+  {
+    name : "Smokin' T's",
+    foodType : "bbq"
+  },
+  {
+    name : "Real Urban Barbecue",
+    foodType : "bbq"
+  }
+]
+
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -6,3 +20,22 @@ function initMap() {
   });
 }
 
+var Restaurant = function(data){
+  this.name = ko.observable(data.name);
+  this.foodType = ko.observable(data.foodType);
+}
+
+var ViewModel = function() {
+  var self = this;
+
+  this.restaurantList = ko.observableArray([]);
+
+  initialRestaurants.forEach(function(item){
+    self.restaurantList.push( new Restaurant(item) );
+  });
+
+  self.currentRestaurant = this.restaurantList()[0];
+  
+}
+
+ko.applyBindings(new ViewModel());
