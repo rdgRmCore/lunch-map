0d7aa9a973ee02994548f88acb3c62f2de97cdab
rdg <rdg@rmcore.com>
2015-08-19 14:41:11 -0500

    Center the map on the office.

--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 42.190220, lng: -87.941175},
+    zoom: 12
   });
 }
 
