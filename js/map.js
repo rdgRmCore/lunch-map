bbf30cca2de407e2d9f39f4c654fa99a675a06db
rdg <rdg@rmcore.com>
2015-08-20 14:36:05 -0500

    Make a side bar and place the map on the right hand side of the screen.

--- js/map.js
+++ js/map.js
@@ -4,30 +4,73 @@
 var initialParks = [
   {
     name : "Illinois Beach State Park",
-    latLng : {lat: 42.423610, lng: -87.805419}
+    latLng : {lat: 42.423610, lng: -87.805419},
+    activities:[
+      "Bike Trails", "Camping", "Cross Country Skiing", "Fishing",
+      "Geocaching", "Hiking Trails", "SCUBA Diving", "Swimming"
+    ]
   },
   {
     name : "Moraine Hills State Park",
-    latLng : {lat: 42.309754, lng: -88.227623}
+    latLng : {lat: 42.309754, lng: -88.227623},
+    activities:[
+      "Bike Trails", "Boating", "Cross Country Skiing", "Fishing",
+      "Geocaching", "Hiking Trails", "Hunting", "Metal Detecting",
+      "Shelter Reservations"
+    ]
   },
   {
     name : "Starved Rock State Park",
-    latLng : {lat: 41.319040, lng: -88.994262}
+    latLng : {lat: 41.319040, lng: -88.994262},
+    activities:[
+      "Boating", "Camping", "Fishing", "Hiking Trails", "Hunting"
+    ]
+  },
+  {
+    name : "Rock Cut State Park",
+    latLng : {lat: 42.342048, lng: -88.973447},
+    activities:[
+      "Bike Trails", "Boating", "Camping", "Cross Country Skiing",
+      "Dog Training", "Equestrian Trails", "Fishing", "Geocaching",
+      "Hiking Trails", "Hunting", "Metal Detecting", "Mountain Bike Trails",
+      "Shelter Reservations", "Swimming"
+    ]
+  },
+  {
+    name : "Chain O'Lakes State Park",
+    latLng : {lat: 42.457765, lng: -88.200289},
+    activities:[
+      "Archery Range", "Bike Trails", "Boating", "Camping",
+      "Cross Country Skiing", "Equestrian Trails", "Fishing",
+      "Hiking Trails", "Hunting", "Metal Detecting", "Shelter Reservations"
+    ]
   }
 ]
 
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+function initialize(){
+  map = new google.maps.Map(document.getElementById("map-canvas"), {
     center: {lat: 42.190220, lng: -87.941175},
     zoom: 8
   });
   parkViewModel.drawMarkers();
 }
 
+var map;
+function initMap() {
+  google.maps.event.addDomListener(window, 'load', initialize);
+/*
+  map = new google.maps.Map(document.getElementById("map-canvas"), {
+    center: {lat: 42.190220, lng: -87.941175},
+    zoom: 8
+  });
+  parkViewModel.drawMarkers();
+*/
+}
+
 var Park = function(data){
   this.name = ko.observable(data.name);
   this.latLng = ko.observable(data.latLng);
+  this.activities = ko.observable(data.activities);
 }
 
 var ViewModel = function() {
@@ -52,3 +95,4 @@ var ViewModel = function() {
 
 var parkViewModel = new ViewModel();
 ko.applyBindings(parkViewModel);
+
